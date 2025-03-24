f565f74eff5c07fe63ca6a469dcedbb1166d28e0
Zahid3640 <zahidnawazpahoor11@gmail.com>
2025-03-24 11:40:24 -0700

    download image code

--- Rust_Projects/download_image_project/src/main.rs
+++ Rust_Projects/download_image_project/src/main.rs
@@ -1,71 +1,43 @@
-// use error_chain::error_chain;
-// use tempfile::Builder;
-// use std::fs::File;
-// use std::io::copy;
-// error_chain! {
-//     foreign_links {
-//         Io(std::io::Error);
-//         HttpRequest(reqwest::Error);
-//     }
-// }
-
-
-// #[tokio::main]
-// async fn main()->Result<()> {
-//    let temp_dir=Builder::new().prefix("Example").tempdir()?;
-//    let target= "https://www.rust-lang.org/logos/rust-logo-512x512.png";
-//    let response=reqwest::get(target).await?;
-//    let mut dest={
-//     let fname=response
-//         .url()
-//         .path_segments()
-//         .and_then(|segments|segments.last())
-//         .and_then(|name|if name.is_empty(){None}else{Some((name))})
-//         .unwrap_or("temp.bin");
-
-
-//     println!("File to Download: '{}'",fname);
-//     let fname=temp_dir.path().join(fname);
-//     println!("Will be Located {:?}",fname);
-//     File::create(fname)?;
-//    };
-
-//    let content=response.text().await?;
-//    copy(&mut content.as_bytes(), &mut dest)?;
-//    Ok(())
-// }
 use error_chain::error_chain;
-use std::io::copy;
 use std::fs::File;
+use std::io::Write;
 use tempfile::Builder;
 
 error_chain! {
-     foreign_links {
-         Io(std::io::Error);
-         HttpRequest(reqwest::Error);
-     }
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;  
     let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    
     let response = reqwest::get(target).await?;
+    
+    // Extract file name from URL
+    let fname = response
+        .url()
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("tmp.bin");
 
-    let mut dest = {
-        let fname = response
-            .url()
-            .path_segments()
-            .and_then(|segments| segments.last())
-            .and_then(|name| if name.is_empty() { None } else { Some(name) })
-            .unwrap_or("tmp.bin");
+    let file_path = tmp_dir.path().join(fname);
+    println!("Downloading file: '{}'", fname);
+    println!("Saving to: {:?}", file_path);
 
-        println!("file to download: '{}'", fname);
-        let fname = tmp_dir.path().join(fname);
-        println!("will be located under: '{:?}'", fname);
-        File::create(fname)?
-    };
-    let content =  response.text().await?;
-    copy(&mut content.as_bytes(), &mut dest)?;
+    // Create file
+    let mut file = File::create(&file_path)?;
+    
+    // Read binary content from response
+    let content = response.bytes().await?;
+    
+    // Write binary data to file
+    file.write_all(&content)?;
+    
+    println!("Download complete!");
     Ok(())
 }
